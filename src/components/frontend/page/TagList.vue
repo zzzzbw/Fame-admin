aa2c46a2052112af7bac738e7b3657f657f8b779
ZBW <920049380@qq.com>
2017-09-12 00:20:02 +0800

    tag滚动

--- src/components/frontend/page/TagList.vue
+++ src/components/frontend/page/TagList.vue
@@ -1,80 +1,25 @@
 <template>
   <div>
     <div class="tag-list">
-      <a class="tag-link">
+      <a class="tag-link" @click="goAnchor('#tag-JavaScript')">
         <span class="tag chip">
             JavaScript
         </span>
       </a>
-      <a class="tag-link">
+      <a class="tag-link" @click="goAnchor('#tag-CSS')">
         <span class="tag chip">
           CSS
         </span>
       </a>
-      <a class="tag-link">
+      <a class="tag-link" @click="goAnchor('#tag-HTML')">
         <span class="tag chip">
           HTML
-        </span>
-      </a>
-      <a class="tag-link">
-        <span class="tag chip">
-            JavaScript
-        </span>
-      </a>
-      <a class="tag-link">
-        <span class="tag chip">
-          CSS
-        </span>
-      </a>
-      <a class="tag-link">
-        <span class="tag chip">
-          HTML
-        </span>
-      </a>
-      <a class="tag-link">
-        <span class="tag chip">
-            JavaScript
-        </span>
-      </a>
-      <a class="tag-link">
-        <span class="tag chip">
-          CSS
-        </span>
-      </a>
-      <a class="tag-link">
-        <span class="tag chip">
-          HTML
-        </span>
-      </a>
-      <a class="tag-link">
-        <span class="tag chip">
-            JavaScript
-        </span>
-      </a>
-      <a class="tag-link">
-        <span class="tag chip">
-          CSS
-        </span>
-      </a>
-      <a class="tag-link">
-        <span class="tag chip">
-          HTML
-        </span>
-      </a>
-      <a class="tag-link">
-        <span class="tag chip">
-            JavaScript
-        </span>
-      </a>
-      <a class="tag-link">
-        <span class="tag chip">
-          CSS
         </span>
       </a>
     </div>
     <div class="tag-content">
       <div class="divider"></div>
-      <div class="tag-title">#JavaScript</div>
+      <div class="tag-title" id="tag-JavaScript">JavaScript</div>
       <ul class="tag-ul">
         <li class="article-title">
           <a>测试标题1</a>
@@ -85,6 +30,147 @@
         <li class="article-title">
           <a>测试标题3</a>
         </li>
+        <li class="article-title">
+          <a>测试标题5</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题6</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题7</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题1</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题2</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题3</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题5</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题6</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题7</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题1</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题2</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题3</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题5</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题6</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题7</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题1</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题2</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题3</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题5</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题6</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题7</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题1</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题2</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题3</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题5</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题6</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题7</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题1</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题2</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题3</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题5</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题6</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题7</a>
+        </li>
+      </ul>
+      <div class="tag-title" id="tag-CSS">CSS</div>
+      <ul class="tag-ul">
+        <li class="article-title">
+          <a>测试标题11</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题12</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题13</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题14</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题15</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题16</a>
+        </li>
+      </ul>
+      <div class="tag-title" id="tag-HTML">HTML</div>
+      <ul class="tag-ul">
+        <li class="article-title">
+          <a>测试标题21</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题22</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题23</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题24</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题25</a>
+        </li>
+        <li class="article-title">
+          <a>测试标题26</a>
+        </li>
       </ul>
     </div>
 
@@ -92,7 +178,47 @@
 </template>
 
 <script type="text/ecmascript-6">
+  let timer = null
+  let isTop = true
+  let osTop = null
 
+  export default {
+    methods: {
+      goAnchor (selector) {
+        let anchor = this.$el.querySelector(selector)
+        let position = anchor.offsetTop
+        let scrollHeight = document.body.scrollHeight
+        let clientHeight = document.body.clientHeight
+        console.log('scrollHeight:' + scrollHeight)
+        console.log('clientHeight:' + clientHeight)
+        console.log('position:' + position)
+        // document.body.scrollTop = anchor.offsetTop
+        // document.documentElement.scrollTop = document.body.scrollTop = anchor.offsetTop
+        timer = setInterval(function () {
+          osTop = document.documentElement.scrollTop = document.body.scrollTop
+          let iSpeed = Math.floor((position - osTop) / 80) || 1// 减速滚动
+          // let iSpeed = -6.5 // 固定速度滚动
+          document.documentElement.scrollTop = document.body.scrollTop = osTop + iSpeed
+          isTop = true
+          console.log(osTop)
+          console.log('clientHeight:' + clientHeight)
+          console.log('scrollHeight:' + scrollHeight)
+          if (osTop === position || osTop + clientHeight === scrollHeight) {
+            clearInterval(timer)
+          }
+        }, 1)
+      }
+    },
+    mounted () {
+      window.onscroll = function () {
+        // 鼠标操作时取消滚动
+        if (!isTop) {
+          clearInterval(timer)
+        }
+        isTop = false
+      }
+    }
+  }
 </script>
 
 <style scoped>
@@ -123,7 +249,7 @@
     transition: all 0.2s;
   }
 
-  .tag-content{
+  .tag-content {
     margin: 20px auto;
     width: 75%;
   }
@@ -133,6 +259,19 @@
     font-size: 24px;
     font-weight: 600;
     color: #34495e;
+  }
+
+  .tag-title:before {
+    content: "#";
+    margin-right: 5px;
+    color: #5764c6;
+    font-size: 1.2em;
+    font-weight: 700;
+  }
+
+  .article-title a {
+    text-decoration: none;
+    font-size: 1.1em;
   }
 
 </style>
